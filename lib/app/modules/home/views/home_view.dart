2e46fb3844860c2863aa47291a38f5bef48a12d5
Azis202017 <azisa6980@gmail.com>
2022-09-07 09:27:58 +0700

    push

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -19,74 +19,79 @@ class HomeView extends GetView<HomeController> {
                 ? Center(
                     child: Lottie.asset('assets/json/loading.json'),
                   )
-                : ListView.builder(
-                    itemCount: controller.surahModel.length,
-                    itemBuilder: (context, index) {
-                      var data = controller.surahModel[index];
-                      return Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 24,
-                        ),
-                        child: Column(
-                          children: [
-                            ListTile(
-                              contentPadding: EdgeInsets.zero,
-                              leading: Container(
-                                width: 50,
-                                margin:
-                                    const EdgeInsets.only(top: 12, bottom: 12),
-                                child: Stack(
+                : RefreshIndicator(
+                    onRefresh: () async {
+                      controller.getSurah();
+                    },
+                    child: ListView.builder(
+                      itemCount: controller.surahModel.length,
+                      itemBuilder: (context, index) {
+                        var data = controller.surahModel[index];
+                        return Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 24,
+                          ),
+                          child: Column(
+                            children: [
+                              ListTile(
+                                contentPadding: EdgeInsets.zero,
+                                leading: Container(
+                                  width: 50,
+                                  margin: const EdgeInsets.only(
+                                      top: 12, bottom: 12),
+                                  child: Stack(
+                                    children: [
+                                      Image.asset(
+                                        'assets/images/muslim_count.png',
+                                        width: 36,
+                                        height: 36,
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(
+                                          top: 3,
+                                        ),
+                                        width: 36,
+                                        child: Text(
+                                          '${data?.number}  ',
+                                          style: Font.leadingTextStyle,
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                title: Text(
+                                  data?.name?.transliteration?.id ?? '',
+                                ),
+                                subtitle: Row(
                                   children: [
-                                    Image.asset(
-                                      'assets/images/muslim_count.png',
-                                      width: 36,
-                                      height: 36,
+                                    Text(
+                                      '${data?.revelation?.id} ',
                                     ),
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                        top: 3,
-                                      ),
-                                      width: 36,
-                                      child: Text(
-                                        '${data?.number}  ',
-                                        style: Font.leadingTextStyle,
-                                        textAlign: TextAlign.center,
+                                    SizedBox(
+                                      width: 4,
+                                      child: CircleAvatar(
+                                        radius: 2,
+                                        backgroundColor: dotColor,
                                       ),
                                     ),
+                                    Text(' ${data?.numberOfVerses} Ayat')
                                   ],
                                 ),
+                                trailing: Text(
+                                  '${data?.name?.short}',
+                                  style: Font.trailingArabicTextStyle
+                                      .copyWith(color: primaryColor),
+                                ),
                               ),
-                              title: Text(
-                                data?.name?.transliteration?.id ?? '',
-                              ),
-                              subtitle: Row(
-                                children: [
-                                  Text(
-                                    '${data?.revelation?.id} ',
-                                  ),
-                                  SizedBox(
-                                    width: 4,
-                                    child: CircleAvatar(
-                                      radius: 2,
-                                      backgroundColor: dotColor,
-                                    ),
-                                  ),
-                                  Text(' ${data?.numberOfVerses} Ayat')
-                                ],
-                              ),
-                              trailing: Text(
-                                '${data?.name?.short}',
-                                style: Font.trailingArabicTextStyle
-                                    .copyWith(color: primaryColor),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            )
-                          ],
-                        ),
-                      );
-                    },
+                              const SizedBox(
+                                height: 20,
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   );
           }),
     );
